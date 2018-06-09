07ab08b16e839226872a0ed8ad7c3d6cee263226
OhmInpw <40101902+OhmInpw@users.noreply.github.com>
2018-06-09 11:28:17 +0200

    Turned the file in to a addition calculator

--- firstProject/main.cpp
+++ firstProject/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
 using namespace std;
-
+int a,b;
+int add(int x, int y);
 int main()
 {
-    cout << "Hello!" << endl;
+    cout << "Lets att two numbers" << endl;
+    cout << "whats the first number?" << endl;
+    cin >> a;
+    cout << endl << "Okey, you chose: "<<a<<endl;
+    cout<<"Whats the second number?"<<endl;
+    cin >> b;
+    cout << endl << "Okey, you chose: "<<b<<endl;
+
+    cout << "The sum of " << a << " and " << b << " is ";
+    cout << add(a,b)<< endl;
     return 0;
+}
+
+int add(int x, int y){
+    return x + y;
 }
